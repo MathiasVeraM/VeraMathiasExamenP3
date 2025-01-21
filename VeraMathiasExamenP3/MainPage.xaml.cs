553242ea19209fc911eb1f5b05b15591e792f338
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-01-21 08:14:05 -0500

    Se guarda y reinicia el programa porque no esta reconociendo el namespace de MainPage.xaml

--- VeraMathiasExamenP3/MainPage.xaml.cs
+++ VeraMathiasExamenP3/MainPage.xaml.cs
@@ -1,17 +1,24 @@
-﻿namespace VeraMathiasExamenP3
+﻿using VeraMathiasExamenP3.Interfaces;
+using VeraMathiasExamenP3.Repositories;
+
+namespace VeraMathiasExamenP3
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
-        public MainPage()
+        private readonly IMVeraPeliculaAPI _mVeraPeliculaAPI;
+        public MainPage(IMVeraPeliculaAPI mVeraPeliculaAPI)
         {
             InitializeComponent();
+            _mVeraPeliculaAPI = mVeraPeliculaAPI;
         }
 
-        private void Buscar_Clicked(object sender, EventArgs e)
+        private async void Buscar_Clicked(object sender, EventArgs e)
         {
+            loading.IsVisible = true;
+            var data = await _mVeraPeliculaAPI.Obtener();
+            listaPeliculas.ItemsSource = data;
 
+            loading.IsVisible = false;
         }
     }
 
