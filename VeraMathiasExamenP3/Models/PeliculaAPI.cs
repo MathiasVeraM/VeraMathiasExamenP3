f4cd11278016bad21679ff713feef2dbfd91ae72
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-01-21 07:29:37 -0500

    Creación del modelo API para obtener todos los datos de la API solicitada, usé el pedido especial JSON a Clases para lograrlo.

---
+++ VeraMathiasExamenP3/Models/PeliculaAPI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeraMathiasExamenP3.Models
+{
+    public class PeliculaAPI
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public int year { get; set; }
+        public string[] genre { get; set; }
+        public float rating { get; set; }
+        public string director { get; set; }
+        public string[] actors { get; set; }
+        public string plot { get; set; }
+        public string poster { get; set; }
+        public string trailer { get; set; }
+        public int runtime { get; set; }
+        public string awards { get; set; }
+        public string country { get; set; }
+        public string language { get; set; }
+        public string boxOffice { get; set; }
+        public string production { get; set; }
+        public string website { get; set; }
+    }
+}
