f873f08f8b7501a273c7d18533ac3410bcc59cba
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-01-21 07:36:20 -0500

    Se creo una interfaz para obtener la lista de las peliculas de la API

---
+++ VeraMathiasExamenP3/Interfaces/IMVeraPeliculaAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeraMathiasExamenP3.Models;
+
+namespace VeraMathiasExamenP3.Interfaces
+{
+    public interface IMVeraPeliculaAPI
+    {
+        public Task<List<MVeraPeliculaAPI>> Obtener();
+    }
+}
