8ae285f7fd83c4293f200546acbf7168b654c85b
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-01-21 23:50:58 -0500

    Se creó el modelo para la base de datos, MovieDb

---
+++ VeraMathiasExamenP3/Models/MovieDb.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeraMathiasExamenP3.Models
+{
+    public class MovieDb
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string MainActor { get; set; }
+        public string Awards { get; set; }
+        public string Website { get; set; }
+        public string MVera { get; set; }
+    }
+}
