f9856d928b2015377a2fa8a25f7e17c973aec9f3
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-01-21 21:52:18 -0500

    Voy a comenzar de 0 de nuevo, desde el modelo, paso a paso. Primero me quiero conectar a la API y mostrar los datos solo de una pelicula. Luego hago el resto

--- VeraMathiasExamenP3/MainPage.xaml.cs
+++ VeraMathiasExamenP3/MainPage.xaml.cs
@@ -1,23 +1,16 @@
-﻿using VeraMathiasExamenP3.Interfaces;
-
+﻿
 namespace VeraMathiasExamenP3
 {
     public partial class MainPage : ContentPage
     {
-        private readonly IMVeraPeliculaAPI _mVeraPeliculaAPI;
-        public MainPage(IMVeraPeliculaAPI mVeraPeliculaAPI)
+        public MainPage()
         {
             InitializeComponent();
-            _mVeraPeliculaAPI = mVeraPeliculaAPI;
         }
 
         private async void Buscar_Clicked(object sender, EventArgs e)
         {
-            Cargando.IsVisible = true;
-            var data = await _mVeraPeliculaAPI.Obtener();
-            listaPeliculas.ItemsSource = data;
-
-            Cargando.IsVisible = false;
+            
         }
     }
 
